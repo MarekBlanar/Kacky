079f1f65e4ac84ba365912d03d0ecf282d38593d
MarekBlanar <xblanarm@stuba.sk>
2022-03-21 18:43:50 +0100

    typy

--- src/test/java/sk/stuba/fei/uim/oop/UP.java
+++ src/test/java/sk/stuba/fei/uim/oop/UP.java
@@ -4,5 +4,14 @@ public class UP {
     public static void main(String[] args){
         System.out.println("Hello world!");
 
+
+        int celeCislo=5;
+        float desatineCislo=5.1f;
+        double desatineVacsie=5.8f;
+        long celeVacsie;
+        char znak='s';
+        String retazec="q";
+
+
     }
 }
