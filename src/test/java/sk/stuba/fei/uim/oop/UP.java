8e8fc63cf2b49e7d70e2c59bda96261edf087330
MarekBlanar <xblanarm@stuba.sk>
2022-03-21 18:46:53 +0100

    Boolean

--- src/test/java/sk/stuba/fei/uim/oop/UP.java
+++ src/test/java/sk/stuba/fei/uim/oop/UP.java
@@ -2,16 +2,9 @@ package sk.stuba.fei.uim.oop;
 
 public class UP {
     public static void main(String[] args){
-        System.out.println("Hello world!");
 
-
-        int celeCislo=5;
-        float desatineCislo=5.1f;
-        double desatineVacsie=5.8f;
-        long celeVacsie;
-        char znak='s';
-        String retazec="q";
-
+        boolean pravda=true;
+        boolean nepravda=false;
 
     }
 }
